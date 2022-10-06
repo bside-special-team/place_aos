76559a7aa3cd49c5116412d3639f492af93d0e6f
JunhwaLee <jhlee@100percent.kr>
2022-10-06 17:02:52 +0900

    위치 권한 추가

--- app/src/main/java/com/special/place/ui/main/MainActivity.kt
+++ app/src/main/java/com/special/place/ui/main/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.special.place.ui.main
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,11 +16,13 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.naver.maps.map.compose.ExperimentalNaverMapApi
-import com.naver.maps.map.compose.NaverMap
-import com.naver.maps.map.compose.rememberCameraPositionState
+import androidx.core.content.ContextCompat
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.LocationSource
+import com.naver.maps.map.compose.*
+import com.naver.maps.map.util.FusedLocationSource
 import com.special.domain.entities.Place
 import com.special.place.toMarker
 import com.special.place.ui.place.register.PlaceRegisterActivity
@@ -25,9 +30,23 @@ import com.special.place.ui.theme.PlaceTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
+const val LOCATION_PERMISSION_REQUEST_CODE = 0xfefefe
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            if (it.all { (_, granted) -> granted }) {
+                //TODO: 퍼미션 수락된 상태
+            } else {
+                showPermissionFailedPopup()
+            }
+        }
+
     private val vm: PlacesViewModel by viewModels()
+
+    private val locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,18 +58,49 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     val chargerList by vm.places.observeAsState(listOf())
-                    MainScaffold(chargerList) {
+                    MainScaffold(locationSource, chargerList) {
                         startActivity(PlaceRegisterActivity.newIntent(this))
                     }
                 }
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        if (permissions.all {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    it
+                ) == PackageManager.PERMISSION_GRANTED
+            }) {
+            // TODO: 권한 수락된 상태!
+        } else {
+            requestPermission.launch(permissions)
+        }
+    }
+
+    private fun showPermissionFailedPopup() {
+        androidx.appcompat.app.AlertDialog.Builder(this)
+            .setCancelable(false)
+            .setMessage("위치 권한이 필요합니다.")
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                requestPermission.launch(permissions)
+            }
+            .setNegativeButton(android.R.string.cancel) { _, _ ->
+                finish()
+            }
+            .show()
+    }
 }
 
 @OptIn(ExperimentalNaverMapApi::class, ExperimentalMaterialApi::class)
 @Composable
-fun MainScaffold(list: List<Place>, registerPlace: () -> Unit) {
+fun MainScaffold(locationSource: LocationSource, list: List<Place>, registerPlace: () -> Unit) {
     val cameraPosition = rememberCameraPositionState()
     val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
         bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
@@ -58,10 +108,6 @@ fun MainScaffold(list: List<Place>, registerPlace: () -> Unit) {
     val coroutineScope = rememberCoroutineScope()
 
     var selectedPlace by remember { mutableStateOf<Place?>(null) }
-
-    if (list.isNotEmpty()) {
-        selectedPlace = list.first()
-    }
 
     BottomSheetScaffold(
         scaffoldState = bottomSheetScaffoldState,
@@ -108,7 +154,10 @@ fun MainScaffold(list: List<Place>, registerPlace: () -> Unit) {
                         bottomSheetScaffoldState.bottomSheetState.collapse()
                     }
                 }
-            }
+            },
+            properties = MapProperties(locationTrackingMode = LocationTrackingMode.NoFollow),
+            locationSource = locationSource,
+            uiSettings = MapUiSettings(isLocationButtonEnabled = true)
         ) {
             list.forEach {
                 it.toMarker {
@@ -125,13 +174,5 @@ fun MainScaffold(list: List<Place>, registerPlace: () -> Unit) {
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PlaceTheme {
-        MainScaffold(listOf()) {}
     }
 }
