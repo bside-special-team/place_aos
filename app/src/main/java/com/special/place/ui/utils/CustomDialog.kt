3d023bf7b222cb2f2e06960db0202c5a2d87f017
Eunjeong <38210019+LeeEunjeong1@users.noreply.github.com>
2022-11-09 15:38:23 +0900

    CustomDialog 디자인 적용

--- app/src/main/java/com/special/place/ui/utils/CustomDialog.kt
+++ app/src/main/java/com/special/place/ui/utils/CustomDialog.kt
@@ -3,6 +3,8 @@ package com.special.place.ui.utils
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ButtonDefaults.elevation
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,76 +15,73 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.special.place.ui.theme.*
 
 @Composable
 fun CustomDialog(
-    value: String, subValue: String, buttonValue:String, setShowDialog: (Boolean) -> Unit, setValue: (String) -> Unit) {
-
-    val txtField = remember { mutableStateOf(value) }
+    value: String, subValue: String, buttonValue:String, subButtonValue:String, setShowDialog: (Boolean) -> Unit, setValue: (String) -> Unit) {
 
     Dialog(onDismissRequest = { setShowDialog(false) }) {
         Surface(
-            shape = RoundedCornerShape(16.dp),
+            shape = RoundedCornerShape(32.dp),
             color = Color.White
         ) {
             Box(
                 contentAlignment = Alignment.Center
             ) {
-                Column(modifier = Modifier.padding(20.dp),
+                Column(modifier = Modifier.padding(start = 24.dp,top = 36.dp, end = 24.dp, bottom = 24.dp),
                 horizontalAlignment = Alignment.CenterHorizontally) {
 
                     Text(
                         text = value,
-                        style = TextStyle(
-                            fontSize = 24.sp,
-                            textAlign = TextAlign.Center
-                        )
+                        style = Title1
                     )
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    Text(
-                        text = subValue,
-                        style = TextStyle(
-                            textAlign = TextAlign.Center
+                    if(subValue!=""){
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = subValue,
+                            style = Subtitle1
                         )
-                    )
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+                    }
+                    Spacer(modifier = Modifier.height(40.dp))
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
                         Button(
                             onClick = {
-                                setValue(txtField.value)
+                                setShowDialog(false)
+                            },
+                            colors = ButtonDefaults.buttonColors(Color.White),
+                            modifier = Modifier
+                                .width(124.dp)
+                                .height(56.dp),
+                            elevation = elevation(
+                                defaultElevation = 0.dp
+                            )
+                        ) {
+                            Text(text = buttonValue,
+                                style = Subtitle2,)
+                        }
+                        Button(
+                            onClick = {
                                 setShowDialog(false)
                             },
                             shape = RoundedCornerShape(20.dp),
+                            colors = ButtonDefaults.buttonColors(Purple500),
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .height(50.dp)
+                                .width(124.dp)
+                                .height(56.dp)
                         ) {
-                            Text(text = buttonValue)
+                            Text(text = subButtonValue,
+                            style = Subtitle2,
+                            color = Color.White)
                         }
-
                     }
-                    Box(modifier = Modifier.padding(40.dp, 10.dp, 40.dp, 0.dp)) {
-                        Button(
-                            onClick = {
-                                setValue(txtField.value)
-                                setShowDialog(false)
-                            },
-                            shape = RoundedCornerShape(20.dp),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(50.dp)
-                        ) {
-                            Text(text = "아니요")
-                        }
 
-                    }
                 }
             }
         }
