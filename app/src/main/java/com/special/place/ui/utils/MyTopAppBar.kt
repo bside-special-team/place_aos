a7f4dec98d4ea4d52c4dbfcf70ab16d009a22ee9
Eunjeong <38210019+LeeEunjeong1@users.noreply.github.com>
2022-11-09 16:02:57 +0900

    TopAppBar

---
+++ app/src/main/java/com/special/place/ui/utils/MyTopAppBar.kt
@@ -0,0 +1,54 @@
+package com.special.place.ui.utils
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.special.place.ui.theme.Subtitle3
+
+@Composable
+fun MyTopAppBar(title:String, navigationType:String, navigationListener: () -> Unit, actionType:String, actionListener:()->Unit) {
+
+    TopAppBar(
+        title = {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = title, style = Subtitle3)
+            }
+
+        },
+        navigationIcon = {
+            when(navigationType){
+                "back" -> {
+                    IconButton(onClick = { navigationListener() }) {
+                        Icon(Icons.Filled.ArrowBack, "backIcon")
+                    }
+                }
+                "close" -> {
+                    IconButton(onClick = { navigationListener() }) {
+                        Icon(Icons.Filled.Close, "closeIcon")
+                    }
+                }
+            }
+        },
+        actions = {
+            when(actionType){
+                "setting" -> {
+                    IconButton(onClick = {actionListener()}) {
+                        Icon(Icons.Filled.Settings,"settingIcon")
+                    }
+                }
+            }
+
+        },
+        backgroundColor = MaterialTheme.colors.background
+    )
+}
