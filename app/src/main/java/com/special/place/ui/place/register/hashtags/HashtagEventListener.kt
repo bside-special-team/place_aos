be9cb8c1142f801a01b80d43bbfd6ec7f3c70632
Eunjeong <38210019+LeeEunjeong1@users.noreply.github.com>
2022-12-06 16:43:17 +0900

    댓글 삭제 요청 UI 추가

--- app/src/main/java/com/special/place/ui/place/register/hashtags/HashtagEventListener.kt
+++ app/src/main/java/com/special/place/ui/place/register/hashtags/HashtagEventListener.kt
@@ -7,15 +7,23 @@ import com.special.place.ui.place.register.RegisterEventListener
 
 interface HashtagEventListener : RegisterEventListener {
     val hashtags: LiveData<List<String>>
+    val editMode: LiveData<Boolean>
 
     fun updateHashtag(hashtag: String)
+    fun updateEditMode(editMode: Boolean)
 
     companion object {
         fun empty() = object : HashtagEventListener {
             override val hashtags: LiveData<List<String>>
                 get() = TODO("Not yet implemented")
+            override val editMode: LiveData<Boolean>
+                get() = TODO("Not yet implemented")
 
             override fun updateHashtag(hashtag: String) {
+                TODO("Not yet implemented")
+            }
+
+            override fun updateEditMode(editMode: Boolean) {
                 TODO("Not yet implemented")
             }
 
